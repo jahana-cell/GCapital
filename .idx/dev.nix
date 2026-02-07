2e4d05e23717f667b1df77d9fb8899afb321e453
Ashif Jahan <jahan.a@growsharecapital.com>
2026-02-07 03:22:48 +0000

    config: add env vars

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -3,26 +3,31 @@
 {pkgs}: {
   # Which nixpkgs channel to use.
   channel = "stable-24.11"; # or "unstable"
+  
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.nodejs_20
+    # pkgs.zulu # OPTIONAL: You can remove this line if you don't need Java/Firebase Emulators to save memory.
     pkgs.zulu
   ];
+  
   # Sets environment variables in the workspace
   env = {};
-  # This adds a file watcher to startup the firebase emulators. The emulators will only start if
-  # a firebase.json file is written into the user's directory
+  
+  # This adds a file watcher to startup the firebase emulators. 
   services.firebase.emulators = {
     # Disabling because we are using prod backends right now
     detect = false;
     projectId = "demo-app";
     services = ["auth" "firestore"];
   };
+  
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
     ];
+    
     workspace = {
       onCreate = {
         default.openFiles = [
@@ -30,9 +35,10 @@
         ];
       };
     };
+    
     # Enable previews and customize configuration
     previews = {
-      enable = true;
+      enable = false; # CHANGED TO FALSE: Prevents "Boot Storm" crash on startup
       previews = {
         web = {
           command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
